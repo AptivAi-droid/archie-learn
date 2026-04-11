e7ddc398078fc0410e75e89c11b95bd67acd067d
Aptiv Consulting <nealtitus4823@gmail.com>
2026-04-11 13:02:57 +0200

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/archie-learn/',
+  base: '/dev/archielearn/',
   plugins: [react()],
   server: {
     proxy: {
